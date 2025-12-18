d4b440ccf095e71629278a3de4b7349e839023c2
UArukonda <uarukonda@gmail.com>
2025-12-18 10:21:03 +0530

    chore: add postgres connection pool

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 const app = require("./app");
+const pool = require("./db/pool");
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+pool
+  .query("SELECT 1")
+  .then(() => {
+    console.log("DB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection failed", err);
+  });
